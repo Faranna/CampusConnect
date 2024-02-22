0acdbb1d7e80c77b389bf1cbb2104d87d6f85e98
farhana <farhana17mar@gmail.com>
2024-02-22 22:44:11 +0600

    password check

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
-const container = document.querySelector(".container"),
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirmPassword]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    // Collect form data
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+ 
+    let message = document.getElementById("message");
+    
+    
+    // Prepare JSON object
+    var data = {
+        "username": username,
+        "password": password,
+        "email": email,
+    };
+
+    // Perform AJAX request using Fetch API
+    fetch('https://abir.nsudesk.tech/cmp/reg', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Registration failed.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle successful registration
+        alert("Registration successful");
+        // Optionally redirect the user to a different page
+        // window.location.href = "success.html";
+        console.log("Registration successful:", data);
+        //window.location.href = 'otp.html';
+
+    })
+    .catch(error => {
+        // Handle registration error
+        alert("Registration failed. Please try again later.");
+        console.error('Error:', error);
+    });
+});
+
+
+
+
+      const container = document.querySelector(".container"),
       pwShowHide = document.querySelectorAll(".showHidePw"),
       pwFields = document.querySelectorAll(".password"),
       signUp = document.querySelector(".signup-link"),
